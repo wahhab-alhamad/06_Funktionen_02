62344b7bbe0782c74b3afa253c7aa7efd87d19a2
wahhab <wahhab911@gmail.com>
2022-01-20 17:28:37 +0100

    hs

--- script_01.js
+++ script_01.js
@@ -7,6 +7,83 @@
 3. Fkt. Grundrechenarten
 4. Ausgabe in Konsole
 */
+// function add(x,y)
+// {
+//     return y + x;   
+// }
+// output(add(2,7));
+// output(sub(2,7));
+// output(multi(2,7));
+// output(divid(112,7));
+// function sub(x,y)
+// {
+//     return x - y;   
+// }
+// function multi(x,y)
+// {
+//     return x * y;    
+// }
+// function divid(x,y)
+// {
+//     if (y == 0)
+//     {
+//         return "ERROR, Division auf 0 kann nicht ausgeführt sein";
+//     }
+//     else 
+//     return x / y;
+//     // oder :
+//     const cond = true;
+//     switch(cond)
+//     {
+//         case (y != 0):
+//         return x/y;
+//         break;
+//     case (y == 0):
+//         return "ERROR, Division auf 0 kann nicht ausgeführt sein";
+//     }
+    
+// }
+function output(outputData)
+{
+    console.log(outputData)
+} 
+output(calc(2,2,"+"));
+output(calc(2,2,"-"));
+output(calc(2,2,"*"));
+output(calc(2,2,":"));
+output(calc(2,0,":"));
+output(calc(2,0,"#!"));
+function calc(x,y,oper)
+{
+    switch(oper)
+    {
+        case "+":
+            return x+y;
+        case "-":
+            return x-y;
+        case "*":
+            return x*y;
+        case ":":
+            if (y==0)
+            return "ERROR, Division auf 0 kann nicht ausgeführt sein";
+            else
+            return x/y;
+        default:
+            "etwas ist schief gelaufen";
+    }
+}
+
+/* output(getOp());
+function getOp() {
+    let op = prompt("Bitte Operator eingeben")
+    return op;  
+}
+ */
+
+
+
+
+
 
 
 
